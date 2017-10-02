24ad9bce168b71955951cf58c000d450046481f6
Alexis Boulet <alexis.boulet@habitat-reflechi.com>
2017-10-02 10:46:15 +0200

    first test with pytest

--- assignments/Session1/test_S2.py
+++ assignments/Session1/test_S2.py
@@ -5,3 +5,8 @@ Created on Mon Oct 02 10:15:41 2017
 @author: alexis
 """
 
+import S1_algotools as algotools
+
+def test_average_above_zero_one_value_under_zero():
+    tab = [-1, 0, 1, 2, 7, 3, 4]
+    assert algotools.average_above_zero(tab) == 3.4
