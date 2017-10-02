0961a502b3bee8077999f5ebdc2bd2527a13e3d8
kingofzeloose <alexisboulet0@gmail.com>
2017-10-02 11:30:32 +0200

    add test average for 0 nmber above zero

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -3,28 +3,12 @@
 @author: bouleta
 @brief a set of generic functions for data management
 """
-"""
-# a variable
-a = 1 # default type : int
-
-# an empty list
-myList = []
-
-# a filled list
-myList2 = [1, 2, 3]
-
-# append to a list
-myList.append(10)
-
-# a buggy list
-myBuggyList = [1, 'a', "Hi"]
-
-# operators
-b = a + 2
-myList_sum = myList+myList2
-"""
 
 def average_above_zero(tableau):
+    """function to find the average of numbers in the tableList    
+    @param list of numbers
+    @return the float representing the average of th numbers
+    """
     #init critical variables    
     positive_values_sum = 0
     positive_values_count = 0
@@ -37,16 +21,18 @@ def average_above_zero(tableau):
             print("this value is null : " + str(item))
         else:
             print("this vale is not positive : " + str(item))
-    average = float(positive_values_sum) / float(positive_values_count)
+    if positive_values_count != 0:
+        average = float(positive_values_sum) / float(positive_values_count)
+    else:
+        average = 0
     # return the result
     return float(average)
 """
-tab = [-1, 0, 1, 2, 7, 3, 4]
+tab = [-1, -5, 0, -2, -7, -3, -4]
 result = average_above_zero(tab)
 message = "The average of positives samples of {list_value} is {res}".format(list_value = tab,res = result)
 print(message)
 """
-
 def max_value(table):
     ##
     # basic fnction able to return the max value of a list
--- assignments/Session1/test_S2.py
+++ assignments/Session1/test_S2.py
@@ -8,5 +8,15 @@ Created on Mon Oct 02 10:15:41 2017
 import S1_algotools as algotools
 
 def test_average_above_zero_one_value_under_zero():
+    """ function to test average_above_zero
+    used to see the aerage if there's 1 or more number under 0
+    """
     tab = [-1, 0, 1, 2, 7, 3, 4]
     assert algotools.average_above_zero(tab) == 3.4
+
+def test_average_above_zero_all_under_or_equal_zero():
+    """ function to test average_above_zero
+    used to see the aerage if there's 1 or more number under 0
+    """
+    tab = [-1, -5, 0, -2, -7, -3, -4]
+    assert algotools.average_above_zero(tab) == 0.0
